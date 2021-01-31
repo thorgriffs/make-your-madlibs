883394d5182ba871a77ca1cb192fa010f60b5385
thorgriffs <22.kelliking@gmail.com>
2021-01-31 10:44:33 -0500

    added routes info to server.js

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const routes = require("./routes");
 
-
-const PORT = process.env.PORT || 3306;
+const htmlRouter = require("./routes/html-routes.js");
+const templatesRouter = require("./routes/templates-api-routes.js");
+const storiesRouter = require("./routes/stories-api-routes");
 
 const app = express();
+const PORT = process.env.PORT || 3306;
 
-app.use(express.static("public"));
+// Require models for syncing
+const db = require("./models");
+
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(routes);
+// Serve static content from the "public" directory
+app.use(express.static("public"));
 
+// Invoke routes
+htmlRouter(app);
+templatesRouter(app);
+storiesRouter(app);
+
+// Start server listening
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
 });
