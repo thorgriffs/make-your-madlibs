57d27aacbc6c1e94ed9dfb726352d4e4255cbc54
thorgriffs <22.kelliking@gmail.com>
2021-01-31 09:43:47 -0500

    added GET route in templates-api-routes.js, renamed html-routes.js

---
+++ routes/templates-api-routes.js
@@ -0,0 +1,13 @@
+const db = require('../models');
+
+// Routes
+module.exports = (app) => {
+  // Get route for retrieving all templates  
+  app.get('/api/templates', (req, res) => {
+    db.Templates.findAll()
+
+    .then((dbTemplates) => res.json(dbTemplates));
+  });
+
+  /* Not sure if we code here for rendering the forms for filling 
+  the blank word spaces... or if that is occurring in another file */
