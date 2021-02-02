954bf43ee8a4d14b158594eb03e9bd8ac730d2f5
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2021-02-02 17:58:02 -0500

    image renamed and path updated

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const htmlRouter = require("./routes/html-routes.js");
+const templatesRouter = require("./routes/templates-api-routes.js");
+const storiesRouter = require("./routes/stories-api-routes");
+
+const app = express();
+const PORT = process.env.PORT || 3306;
+
+// Require models for syncing
+const db = require("./models");
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Serve static content from the "public" directory
+app.use(express.static("public"));
+
+// Invoke routes
+htmlRouter(app);
+templatesRouter(app);
+storiesRouter(app);
+
+// Start server listening
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
