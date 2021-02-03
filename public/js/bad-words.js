8eb1ff01a82e61007516994baed020297da93fef
Mengmei Tu <redmmeii@gmail.com>
2021-02-02 21:10:47 -0500

    update code, need to add input id later

--- public/js/bad-words.js
+++ public/js/bad-words.js
@@ -2,9 +2,12 @@
 const Filter = require('bad-words');
 
 $(function () {
-    //function on filtering the inputs
-    let allInputs = $(":input");
-    filter = new Filter(allInputs);
+    //function on filtering the inputs for bad words
+    //need to replace :input by grabbing the input id on each word
+    let filteredNoun = new Filter(":input");
+    let filteredAdjective = new Filter(":input");
+    let filteredPluralNoun = new Filter(":input");
+    let filteredVerb = new Filter(":input");
 });
 
 
