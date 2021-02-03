fb40ae4a5b90390d8ce49b4c5958341ef4fe7900
Mengmei Tu <redmmeii@gmail.com>
2021-02-03 17:02:16 -0500

    write on click function on button

--- public/js/bad-words.js
+++ public/js/bad-words.js
@@ -1,16 +1,18 @@
 // dependencies
 const Filter = require('bad-words');
 
-$(function () {
-    //function on filtering the inputs for bad words
-    //need to replace :input by grabbing the input id on each word
-    let cleanNoun = filter.clean(new Filter(":input"));
-    let cleanAdjective = filter.clean(new Filter(":input"));
-    let cleanPluralNoun = filter.clean(new Filter(":input"));
-    let cleanVerb = filter.clean(new Filter(":input"));
+// console.log(filter.clean("asshole"));
 
+// console.log(filter.isProfane("asshole"));
 
-
+//function on filtering the inputs for bad words
+//need to replace :input by grabbing the input id on each word
+$(":button").click(function () {
+    filter = new Filter();
+    if (filter.isProfane(":input")) {
+        alert("Oops, no profanity is allowed!");
+    } else {
+        //fill in here to submit form to connect with db template
+    }
 });
-
 
