5fe19ddc8475044d5e71897dacf2801701f732f5
munozjuan <munoz.juan07@hotmail.com>
2021-02-02 21:42:31 -0500

    added madlibs.js files

---
+++ public/js/madlibs.js
@@ -0,0 +1,10 @@
+const mtp = require('madlibs-template-parser')
+ 
+
+console.log(
+
+mtp("The <pluralNoun> brown <pluralNoun> <pluralNoun> over the lazy <pluralNoun> and shouted,That was easy!"))
+
+
+ 
+
