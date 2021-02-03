e439dde7cd2d5b939c14dd5241b51ab265afdcf9
Mengmei Tu <redmmeii@gmail.com>
2021-02-02 21:15:41 -0500

    update codes

--- public/js/bad-words.js
+++ public/js/bad-words.js
@@ -4,10 +4,13 @@ const Filter = require('bad-words');
 $(function () {
     //function on filtering the inputs for bad words
     //need to replace :input by grabbing the input id on each word
-    let filteredNoun = new Filter(":input");
-    let filteredAdjective = new Filter(":input");
-    let filteredPluralNoun = new Filter(":input");
-    let filteredVerb = new Filter(":input");
+    let cleanNoun = filter.clean(new Filter(":input"));
+    let cleanAdjective = filter.clean(new Filter(":input"));
+    let cleanPluralNoun = filter.clean(new Filter(":input"));
+    let cleanVerb = filter.clean(new Filter(":input"));
+
+
+
 });
 
 
