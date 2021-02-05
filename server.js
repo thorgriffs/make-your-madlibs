b89c7cdef28ea2aa2750859a85ee6607823b46f3
munozjuan <munoz.juan07@hotmail.com>
2021-02-04 22:29:03 -0500

    server.js

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const { Sequelize } = require('sequelize');
 
-const htmlRouter = require("./routes/html-routes.js");
-const templatesRouter = require("./routes/templates-api-routes.js");
-const storiesRouter = require("./routes/stories-api-routes");
+// const htmlRouter = require("./routes/html-routes.js");
+// const templatesRouter = require("./routes/templates-api-routes.js");
+// const storiesRouter = require("./routes/stories-api-routes");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Require models for syncing
-const db = require('./models');
+const db = require("./models");
 
 app.use(express.static("public"));
 
@@ -25,11 +25,10 @@ app.use(express.json());
 // Serve static content from the "public" directory
 app.use(express.static("public"));
 
-
 // // Invoke routes
 require("./routes/html-routes.js")(app);
-templatesRouter(app);
-storiesRouter(app);
+// templatesRouter(app);
+// storiesRouter(app);
 
 // Start server listening
 db.sequelize.sync({ force: true }).then(() => {
