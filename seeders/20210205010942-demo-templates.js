8280f09c37231c66df4e21ae4160ca6f59a06c47
thorgriffs <22.kelliking@gmail.com>
2021-02-04 23:37:47 -0500

    updated seeders with Talk Like a Pirate template

--- seeders/20210205010942-demo-templates.js
+++ seeders/20210205010942-demo-templates.js
@@ -5,7 +5,8 @@ module.exports = {
 
     await queryInterface.bulkInsert('Templates', [{
       title: "Talk Like A Pirate",
-      templateBody: "Ye can always pretend to be a bloodthirsty <singularNoun>, threatening everyone by waving yer <descriptiveAdjective> sword in the air, but until ye learn to <rootVerb> like a pirate, ye'll never be <mannerAdverb> accepted as an authentic <singularNount>.",
+      teaser: "Ye can always pretend to be a bloodthirsty...",
+      templateBody: "Ye can always pretend to be a bloodthirsty <singularNoun>, threatening everyone by waving yer <descriptiveAdjective> sword in the air, but until ye learn to <rootVerb> like a pirate, ye'll never be <mannerAdverb> accepted as an authentic <singularNount>. So here's what ye do: Cleverly work into yer daily conversations <descriptiveAdjective> pirate phrases such as \"Ahoy there, <pluralNound>,\" \"Avast, ye <pluralNoun>,\" and \"Shiver me <pluralNoun>.\" Remember to drop all yer g's when ye say such words as \"sailin'\", \"spittin'\", and \"fightin'\".  This will give ye a/an <singularNoun> start to being recognized as a swashbucklin' <singularNoun>.  Once ye have the lingo down pat, it helps to wear a three-cornered <singularNoun> on yer head, stash a/an <singularNoun> in yer pants, and keep a/an <singularNoun> perched atop yer <singularNoun>. Aye, now ye be a real pirate!",
       createdAt: new Date(),
       updatedAt: new Date()
     }], {});
