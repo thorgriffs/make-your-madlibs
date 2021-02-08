20f273bec37db9fc7f0df7b88c308e071cc39b65
Mengmei Tu <redmmeii@gmail.com>
2021-02-08 18:46:15 -0500

    clean up api-routes codes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,15 +4,14 @@ const db = require("../models");
 const madlibs = require("../lib/madlibs");
 
 // Routes
-// Each of the below routes just handles the HTML page that the user gets sent to.
 
 // GET route on index page to display story title and teaser
 router.get("/", async (req, res) => {
   // retrieve all but only display title and teaser
   try {
-    // console.log("before await");
     const templates = await db.Templates.findAll({ raw: true });
-    // console.log(templates);
+
+    // render to index handlesbars
     res.render("index", { templates });
   } catch (err) {
     console.log("An error occurred:", err);
@@ -41,34 +40,16 @@ router.get("/create/:id/:title", async (req, res) => {
   }
 });
 
-// Render the blanks in a form via handlebars (each helper)
-
 // POST route for creating story and create in db
 router.post("/create/:id/:title", async (req, res) => {
   try {
-    // req.body // { "1": "word", "2": "formId"}
-    console.log(req.body);
-    // let templateId;
-    // const blanks = [];
-    // for (const field in req.body) {
-    //   const number = Number(field);
-    //   if (isNaN(number)) {
-    //     // eslint-disable-next-line no-unused-vars
-    //     templateId = req.body[field];
-    //   } else {
-    //     blanks[number] = req.body[field];
-    //   }
-    // }
-
     // use formStory function from madlibs.js
     const completedStory = await madlibs.formStory(req.params.id, req.body);
-
     const createStory = await db.Stories.create({
       title: req.params.title,
       storyBody: completedStory,
     });
 
-    // res.render("result", createStory);
     // Redirect to show completed story
     res.redirect("/result/" + createStory.id);
   } catch (err) {
