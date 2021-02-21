20627150aff8a557803289b58641d821a5e8edb8
munozjuan <munoz.juan07@hotmail.com>
2021-02-21 14:56:28 -0500

    working on login

--- models/login.js
+++ models/login.js
@@ -1,19 +1,47 @@
-//create stories model
-module.exports = (sequelize, DataTypes) => {
-    const Stories = sequelize.define("Stories", {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      storyBody: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
-      upvotes: {
-        type: DataTypes.INTEGER,
-      },
-    });
+var sequelize = require('sequelize');
+var bcrypt  = require('bcrypt');
+
+const sequelize = new sequelize('ourDatabase', 'root', 'root'{
+  host: 'localhost',
+  port: 8080,
+  dialect:'mysql',
+  pool: {
+      max: 5,
+      min: 0,
+      aquire: 30000,
+      idle: 1000
+  },
+  operatorsAliases: false
   
-    return Stories;
-  };
+});
+
+//set up user table
+
+var user = sequelize.define('users'{
+  id:{
+    type: sequelize.INTEGER,
+    unique: true,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username:{
+    type: sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password:{
+    type: sequelize.STRING,
+    allowNull: false
+  }
+});
+
+user.beforeCreate((user, options) => {
+  const salt = bcrypt.genSaltSync();
+  user.password = bcrypt.hashSync(user.password, salt);
+
+});
+
+user.prototype.validPassword = function(password){
   
+}
