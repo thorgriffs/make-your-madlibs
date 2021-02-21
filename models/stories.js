8291de1faf30b0f02f576a85a02f6004491ea4fc
munozjuan <munoz.juan07@hotmail.com>
2021-02-20 22:44:38 -0500

    added files

--- models/stories.js
+++ models/stories.js
@@ -1,20 +1,18 @@
 //create stories model
 module.exports = (sequelize, DataTypes) => {
-    const Stories = sequelize.define('Stories', {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        storyBody: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        upvotes: {
-            type: DataTypes.INTEGER,
-        }
+    const Stories = sequelize.define("Stories", {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      storyBody: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      upvotes: {
+        type: DataTypes.INTEGER,
+      },
     });
-
+  
     return Stories;
-}
-
-
+  };
