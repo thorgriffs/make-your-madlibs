8212989b9a459d062a595ea51ff4909d582f50e5
munozjuan <munoz.juan07@hotmail.com>
2021-02-23 17:59:41 -0500

    added route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 const madlibs = require("../lib/madlibs");
+var db = require("../models");
+var passport = require("../config/passport");
 
 
 
