fd1c81cbaba82654610718d464f9bf5f181e0471
munozjuan <munoz.juan07@hotmail.com>
2021-02-23 21:10:43 -0500

    added

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
-var session = require("express-session");
-var passport = require("./config/passport");
+const passport = require("./config/passport");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 
 // We need to use sessions to keep track of our user's login status
+// Creating express app and configuring middleware needed for authentication
+var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  });
+});
+
+
 
 // Require models for syncing
 const db = require("./models");
@@ -28,7 +47,8 @@ app.set("view engine", "handlebars");
 // Serve static content from the "public" directory
 app.use(express.static("public"));
 
-// Invoke routes
+//routes
+
 const routes = require("./routes/api-routes");
 app.use(routes);
 
