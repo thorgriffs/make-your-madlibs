8a5eb5262b0b3146773406bc86a6984d62f44646
munozjuan <munoz.juan07@hotmail.com>
2021-02-23 22:59:17 -0500

    ok

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8080;
 
 // We need to use sessions to keep track of our user's login status
 // Creating express app and configuring middleware needed for authentication
-var app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -17,18 +17,6 @@ app.use(express.static("public"));
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
-
-// Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-  });
-});
-
 
 
 // Require models for syncing
@@ -48,11 +36,22 @@ app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
 //routes
-
+/*
 const routes = require("./routes/api-routes");
-app.use(routes);
+//
+const routes = require("./routes/html-routes.js");
+const routes = require("./routes/api-routes.js");
+//
+app.use(routes); */
 
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
 // Start server listening
+
+// Syncing our database and logging a message to the user upon success
+
 db.sequelize.sync({}).then(() => {
   app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 });
+
